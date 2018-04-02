455f518a151ec780dbd6de16850a627d9eec6e33
Cuong Nguyen Cao <wind.ncc.1996@gmail.com>
2018-04-02 10:53:11 +0700

    add setting static root

--- football/settings.py
+++ football/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static/'
 # Rest framework config
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
